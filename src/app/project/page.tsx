0505801c710ef7c0971dabb9761c0a40d2f87dee
shippudev <nitishdev0101@gmail.com>
2024-06-26 11:30:22 +0530

    project page responsiveness completed

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -2,16 +2,16 @@ import ProjectCard from '@/components/ProjectCard';
 
 const ProjectMain = () => {
   return (
-    <div className="relative  pt-[12.5rem]">
+    <div className="relative pt-20 md:pt-[12.5rem]">
       <div className="w-full h-full absolute overflow-hidden bottom-0">
         <div className="proShape1"></div>
         <div className="proShape2"></div>
         <div className="proShape3"></div>
         <div className="proShape4"></div>
       </div>
-      <div className="relative px-24">
-        <div className='px-40'>
-        <h4 className='text-6xl font-bold leading-snug'>
+      <div className="relative px-3 md:px-24">
+        <div className='px-3 md:px-40'>
+        <h4 className='text-3xl md:text-6xl text-center md:text-start font-bold leading-snug'>
 The work we do, <br />
 and the people we help.</h4>
         </div>
